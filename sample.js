ca82522937ab264b63fbb83ffdb4e66b1d614c48
Massimo <37666636+Supergiovane@users.noreply.github.com>
2022-02-07 11:12:21 +0100

    .

--- sample.js
+++ sample.js
@@ -1,217 +1,69 @@
-const KNXClient = require("./KNXUltimate");
+
+const knx = require("./index.js");
 const KNXAddress = require("./src/protocol/KNXAddress").KNXAddress;
 const DPTLib = require('./src/dptlib');
 const KNXDataBuffer = require("./src/protocol/KNXDataBuffer").KNXDataBuffer;
 
 // Create tunnel socket with source knx address 1.1.100
-const optionsDefaults = {
-    physAddr: '15.15.21',
-    connectionKeepAliveTimeout: 60,
 
-    //ipAddr: "224.0.23.12",
-    //hostProtocol: "Multicast", // TunnelUDP, TunnelTCP, Multicast
-    //isSecureKNXEnabled: false,
 
-    ipAddr: "192.168.1.15",
-    hostProtocol: "TunnelUDP", // TunnelUDP, TunnelTCP, Multicast
-    isSecureKNXEnabled: false,
-
-    //ipAddr: "192.168.1.54",
-    //hostProtocol: "TunnelTCP", // TunnelUDP, TunnelTCP, Multicast
-    //isSecureKNXEnabled: true,
-
-    ipPort: 3671,
+// 25/08/2021 Moved out of node.initKNXConnection 
+let knxUltimateClientProperties = {
+    ipAddr: "224.0.23.12",
+    ipPort: "3671",
+    physAddr: "1.1.100",
     suppress_ack_ldatareq: false,
-    loglevel: "info",
-    localEchoInTunneling: true,
-    //localIPAddress: "", // Riempito da KNXEngine
-    interface: "" // en3
-};
-const knxClient = new KNXClient.KNXClient(optionsDefaults);
-
-knxClient.on(KNXClient.KNXClientEvents.error, err => {
-
-    console.log("BANANA ERRORE", err);
-
-});
-
-// Call discoverCB when a knx gateway has been discovered.
-knxClient.on(KNXClient.KNXClientEvents.discover, info => {
-    const [ip, port] = info.split(":");
-    discoverCB(ip, port);
-});
-
-knxClient.on(KNXClient.KNXClientEvents.disconnected, info => {
-
-    console.log("BANANA DISCONNESSO", info);
-
-});
-
-knxClient.on(KNXClient.KNXClientEvents.close, info => {
-
-    console.log("BANANA CHIUSO", info);
-
-});
-
-knxClient.on(KNXClient.KNXClientEvents.connected, info => {
-
-    console.log("CONNESSO CON CHANNEL ID ", knxClient.channelID);
-
-});
-
-knxClient.on(KNXClient.KNXClientEvents.connecting, info => {
-
-    console.log("CONNECTING ", info);
-
-});
-
-
-// start auto discovery on interface with ip 192.168.1.99
-//knxClient.startDiscovery("192.168.1.157");
-
-const wait = (t = 3000) => {
-    return new Promise(resolve => {
-        setTimeout(() => { resolve(); }, t);
-    });
+    loglevel: "error", // or "debug" is the default
+    localEchoInTunneling: true, // Leave true, forever.
+    hostProtocol: "Multicast", // "Multicast" in case you use a KNX/IP Router, "TunnelUDP" in case of KNX/IP Interface, "TunnelTCP" in case of secure KNX/IP Interface (not yet implemented)
+    isSecureKNXEnabled: false, // Leave "false" until KNX-Secure has been released
+    jKNXSecureKeyring: "", // ETS Keyring JSON file (leave blank until KNX-Secure has been released)
+    localIPAddress: "", // Leave blank, will be automatically filled by KNXUltimate
+    KNXEthInterface: "Auto", // Bind to the first avaiable local interfavce. "Manual" if you wish to specify the interface (for example eth1); in this case, set the property interface to the interface name (interface:"eth1")
 };
 
-const handleBusEvent = function (_datagram, _echoed, rawCEMISocketMessage) {
-    //console.log(`${srcAddress.toString()} -> ${dstAddress.toString()} :`, npdu.dataValue);
-    try {
-        var dpt = DPTLib.resolve("DPT1.001");
-        var jsvalue = DPTLib.fromBuffer(_datagram.cEMIMessage.npdu.dataValue, dpt)
-    } catch (error) {
-        console.log(error);
-    }
 
-    let sType = "";
-    if (_datagram.cEMIMessage.npdu.isGroupRead) sType = "GroupValue_Read";
-    if (_datagram.cEMIMessage.npdu.isGroupResponse) sType = "GroupValue_Response";
-    if (_datagram.cEMIMessage.npdu.isGroupWrite) sType = "GroupValue_Write";
+// Let's go
+const knxUltimateClient = new knx.KNXClient(knxUltimateClientProperties);
 
-    let srcAddress = _datagram.cEMIMessage.srcAddress.toString();
-    let dstAddress = _datagram.cEMIMessage.dstAddress.toString();
+// Setting handlers
+// ######################################
+knxUltimateClient.on(knx.KNXClient.KNXClientEvents.indication, handleBusEvents);
+knxUltimateClient.on(knx.KNXClient.KNXClientEvents.error, err => {
+    // Error event
+    console.log("Error", err)
+});
+knxUltimateClient.on(knx.KNXClient.KNXClientEvents.disconnected, info => {
+    // The client is cisconnected
+    console.log("Disconnected", info)
+});
+knxUltimateClient.on(knx.KNXClient.KNXClientEvents.close, info => {
+    // The client connection has been closed
+    console.log("Closed", info)
 
-    let isRepeated = _datagram.cEMIMessage.control.repeat === 1 ? false : true;
+});
+knxUltimateClient.on(knx.KNXClient.KNXClientEvents.connected, info => {
+    // The client is connected
+    console.log("Connected. On Duty", info)
 
+});
+knxUltimateClient.on(knx.KNXClient.KNXClientEvents.connecting, info => {
+    // The client is setting up the connection
+    console.log("Connecting...", info)
+});
+// ######################################
 
-    // 23/03/2021 Supergiovane: Added the CEMI telegram for ETS Diagnostic
-    // #####################################################################
-    let cemiETS = "";
+knxUltimateClient.Connect();
 
-    if (rawCEMISocketMessage !== undefined) {
-        try {
-            var iStart = _datagram._header._headerLength + 4;
-            cemiETS = rawCEMISocketMessage.toString("hex").substring(iStart * 2);
-        } catch (error) { cemiETS = ""; }
-    }
+// Handle BUS events
+// ---------------------------------------------------------------------------------------
+function handleBusEvents(_datagram, _echoed) {
 
+    // Traffic
+    let _evt = "";
+    if (_datagram.cEMIMessage.npdu.isGroupRead) _evt = "GroupValue_Read";
+    if (_datagram.cEMIMessage.npdu.isGroupResponse) _evt = "GroupValue_Response";
+    if (_datagram.cEMIMessage.npdu.isGroupWrite) _evt = "GroupValue_Write";
+    console.log("src: " + _datagram.cEMIMessage.srcAddress.toString() + " dest: " + _datagram.cEMIMessage.dstAddress.toString(), " event: " + _evt);
 
-    // #####################################################################
-
-    console.log("BANANA", srcAddress, dstAddress, jsvalue, "Echoed", _echoed, "Type", sType, "Repeat", isRepeated, "cemiETS", cemiETS)
-
-};
-
-try {
-    console.log("INIZIO CONNESSIONE") 
-    knxClient.Connect();
-
-} catch (error) {
-    console.log("ORRORE CONNESSIONE",error);
-    return;
 }
-
-
-knxClient.on(KNXClient.KNXClientEvents.indication, handleBusEvent);
-
-setTimeout(() => {
-    try {
-
-        console.log("ACCENDEO")
-        knxClient.write("0/1/1", true, "DPT1.001");
-    } catch (error) {
-        console.log("ORRORE", error)
-    }
-
-}, 5000);
-
-setTimeout(() => {
-
-    //grpaddr, value, bitlength
-
-    // Write raw buffer to a groupaddress with dpt 1 (e.g light on = value true = Buffer<01>) with a bitlength of 1
-    let writeraw = Buffer.from('01', 'hex'); // Put '00' instead of '01' to switch off the light.
-    let bitlenght = 1;
-
-    try {
-        console.log("RAWEO", writeraw);
-        knxClient.writeRaw("0/1/1", writeraw, bitlenght);
-
-    } catch (error) {
-        console.log("ORRORE", error)
-    }
-
-}, 8000);
-
-setTimeout(() => {
-
-    console.log("DISCONNETTO");
-    knxClient.Disconnect();
-   
- 
-}, 10000);
-
-setTimeout(() => {
-
-      
-    console.log("CONNETTO");
-    knxClient.Connect();
-
-}, 12000);
-
-// setInterval(() => {
-//     try {
-//         let adpu = {};
-//         let knxDataBuff
-//         let addr
-//         adpu = DPTLib.populateAPDU(false, adpu, "DPT1.001");
-//         knxDataBuff = new KNXDataBuffer(adpu.data)
-//         addr = KNXAddress.createFromString("0/1/1", KNXAddress.TYPE_GROUP); // 257
-//         console.log("BANANA SOENGO)")
-//         knxEngine.writeAsync(addr, knxDataBuff);
-//     } catch (error) {
-//         console.log("ORRORE", error)
-//     }
-// }, 3000);
-
-// setTimeout(() => {
-//     knxClient.readAsync(addr);
-
-// }, 10000);
-// setTimeout(() => {
-//     knxClient.respondAsync(addr, knxDataBuff);
-// }, 5000);
-
-
-
-
-
-// Actions to perform when a KNX gateway has been discovered.
-const discoverCB = (ip, port) => {
-    // console.log("Connecting to ", ip, port);
-
-    // // Connect to the knx gateway on ip:port
-    // knxClient.connectAsync(ip, port)
-    //     .then(() => {
-    //         console.log("Connected through channel id ", knxClient.channelID);
-
-    //         var dpt = DPTLib.resolve("DPT1.001");
-    //         let adpu = {};
-
-    //         console.log("SENT", DPTLib.populateAPDU(false, adpu, dpt))
-    //         //knxClient.sendWriteRequest("15.15.0", "0/1/1", DPTLib.populateAPDU(false, "", dpt));
-
-    //     });
-
-};
