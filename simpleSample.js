f3aa516b50f7f8ba10038dfccea79376ded88b2a
Massimo <37666636+Supergiovane@users.noreply.github.com>
2022-02-08 08:59:07 +0100

    .

---
+++ simpleSample.js
@@ -0,0 +1,41 @@
+
+const knx = require("./index.js");
+
+// Set the properties
+let knxUltimateClientProperties = {
+    ipAddr: "224.0.23.12",
+    ipPort: "3671",
+    physAddr: "1.1.100",
+    suppress_ack_ldatareq: false,
+    loglevel: "error", // or "debug" is the default
+    localEchoInTunneling: true, // Leave true, forever.
+    hostProtocol: "Multicast", // "Multicast" in case you use a KNX/IP Router, "TunnelUDP" in case of KNX/IP Interface, "TunnelTCP" in case of secure KNX/IP Interface (not yet implemented)
+    isSecureKNXEnabled: false, // Leave "false" until KNX-Secure has been released
+    jKNXSecureKeyring: "", // ETS Keyring JSON file (leave blank until KNX-Secure has been released)
+    localIPAddress: "", // Leave blank, will be automatically filled by KNXUltimate
+    KNXEthInterface: "Auto", // Bind to the first avaiable local interfavce. "Manual" if you wish to specify the interface (for example eth1); in this case, set the property interface to the interface name (interface:"eth1")
+};
+
+
+// Instantiate the client
+const knxUltimateClient = new knx.KNXClient(knxUltimateClientProperties);
+
+// Setting handlers
+knxUltimateClient.on(knx.KNXClient.KNXClientEvents.indication, function (_datagram, _echoed) {
+
+    // Traffic
+    let _evt = "";
+    if (_datagram.cEMIMessage.npdu.isGroupRead) _evt = "GroupValue_Read";
+    if (_datagram.cEMIMessage.npdu.isGroupResponse) _evt = "GroupValue_Response";
+    if (_datagram.cEMIMessage.npdu.isGroupWrite) _evt = "GroupValue_Write";
+    console.log("src: " + _datagram.cEMIMessage.srcAddress.toString() + " dest: " + _datagram.cEMIMessage.dstAddress.toString(), " event: " + _evt);
+
+});
+
+// Connect
+knxUltimateClient.Connect();
+
+// WARNING, THIS WILL WRITE ON YOUR KNX BUS!
+knxUltimateClient.write("0/1/1", false, "1.001");
+
+
