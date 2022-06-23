c89f8915cf60dcf235c3bcb0646f81bd790833d0
Massimo <37666636+Supergiovane@users.noreply.github.com>
2022-06-23 18:31:34 +0200

    .

--- src/protocol/KNXSecureSessionResponse.js
+++ src/protocol/KNXSecureSessionResponse.js
@@ -11,34 +11,29 @@ const CRIFactory = __importDefault(require("./CRIFactory"));
 const knx = require("../../index.js");
 
 class KNXSecureSessionResponse extends KNXPacket.KNXPacket {
-    constructor(cri, hpaiData, secureSessionID, messageAuthenticationCode) {
-        //super(KNXConstants.KNX_CONSTANTS.SECURE_SESSION_REQUEST, hpaiControl.length + hpaiData.length + cri.length + 32);
-        super(KNXConstants.KNX_CONSTANTS.SECURE_SESSION_RESPONSE, hpaiData.length + 32);
-        this.cri = cri;
-        this.hpaiData = hpaiData;
+    constructor(secureSessionID, diffieHellmanServerPublicValue, messageAuthenticationCode) {
 
-        // SONO ARRIVATO QUI 23/06/2022
+        //super(KNXConstants.KNX_CONSTANTS.SECURE_SESSION_REQUEST, hpaiControl.length + hpaiData.length + cri.length + 32);
+        super(KNXConstants.KNX_CONSTANTS.SECURE_SESSION_RESPONSE, 56);
+        this.secureSessionID = secureSessionID;
+        this.diffieHellmanServerPublicValue = diffieHellmanServerPublicValue;
+        this.messageAuthenticationCode = messageAuthenticationCode;
+
+       
 
         // 8 octets for the UDP/TCP HPAI and 32 octets for the client’s ECDH public value
         // LENGTH: Final = 56 bytes
-        // KNX/IP Session Response #0001
-        // KNX/IP Header: Session Response
-        //     Header Length: 6 bytes
-        //     Protocol Version: 1.0
-        //     Service Identifier: Session Response (0x0952)
-        //     Total Length: 56 bytes
-        // Session: 0x0001
-        // DH Server Public Value: $ E9 93 E4 0C BA A3 D1 67 8D 88 F0 E9 7D 61 36 1E 35 44 F7 4E 60 0D 01 F9 1C 42 0D 16 30 B9 76 5F
-        // Message Authentication Code: $ EA 34 CA 97 D5 7D D5 DE E7 A2 DE 58 86 6B 5A 4B
+
 
         // Get the Keyring
         //this.keyring = knx.getDecodedKeyring();
+
         // Add the Diffie-Hellman
-        let Curve25519 = require('./../Curve25519');
-        const crypto = require('crypto');
-        let keyPair = Curve25519.generateKeyPair(crypto.randomBytes(32));
-        // Get the Keyring
-        this.keyring = knx.getDecodedKeyring();
+        // let Curve25519 = require('./../Curve25519');
+        // const crypto = require('crypto');
+        // let keyPair = Curve25519.generateKeyPair(crypto.randomBytes(32));
+        // // Get the Keyring
+        // this.keyring = knx.getDecodedKeyring();
 
         // // Send the DH curve as well
         // // 02/01/2022 SONO ARRIVATO QUI get the authentication password from the first tunnel of the interface
@@ -65,16 +60,6 @@ class KNXSecureSessionResponse extends KNXPacket.KNXPacket {
     }
     static createFromBuffer(buffer, offset = 0) {
 
-        // +-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+---------------------+
-        // |         Header Length         |        Protocol Version       |                     |
-        // |         (06h)                 |        (10h)                  |                     |
-        // +-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+                     |
-        // |                     Session Type Identifier                   |  KNXnet/IP secure   |
-        // |                     (0952h)                                   |  Header             |
-        // +-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+                     |
-        // |                           Total Length                        |                     |
-        // |                           (38h)                               |                     |
-        // +-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+---------------------+
 
         // +-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+---------------------+
         // |                     Secure Session Identifier                 |                     |
@@ -87,16 +72,27 @@ class KNXSecureSessionResponse extends KNXPacket.KNXPacket {
         // |                   (16 Octet)                                  |  (AES128 CCM)       |
         // +-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+-7-+-6-+-5-+-4-+-3-+-2-+-1-+-0-+---------------------+
 
+        // From Wireshark
+        // KNX/IP Session Response #0001
+        // KNX/IP Header: Session Response
+        //     Header Length: 6 bytes
+        //     Protocol Version: 1.0
+        //     Service Identifier: Session Response (0x0952)
+        //     Total Length: 56 bytes
+        // Session: 0x0001
+        // DH Server Public Value: $ E9 93 E4 0C BA A3 D1 67 8D 88 F0 E9 7D 61 36 1E 35 44 F7 4E 60 0D 01 F9 1C 42 0D 16 30 B9 76 5F
+        // Message Authentication Code: $ EA 34 CA 97 D5 7D D5 DE E7 A2 DE 58 86 6B 5A 4B
 
         if (offset >= buffer.length) {
             throw new Error('Buffer too short');
         }
-        const hpaiControl = HPAI.HPAI.createFromBuffer(buffer, offset);
-        offset += hpaiControl.length;
-        const hpaiData = HPAI.HPAI.createFromBuffer(buffer, offset);
-        offset += hpaiData.length;
-        const cri = CRIFactory.default.createFromBuffer(buffer, offset);
-        return new KNXSecureSessionResponse(cri, hpaiControl, hpaiData);
+        // Secure Session Identifier  
+        const secureSessionID = Buffer.from(buffer.slice(0, 2)).toString("hex");//Buffer.from(buffer, 0, 2).readUint16LE();
+        // Diffie-Hellman Server Public Value Y    
+        const diffieHellmanServerPublicValue = Buffer.from(buffer.slice(2, 34)).toString("hex");
+        // Message Authentication Code
+        const messageAuthenticationCode = Buffer.from(buffer.slice(34, 50)).toString("hex");
+        return new KNXSecureSessionResponse(secureSessionID, diffieHellmanServerPublicValue, messageAuthenticationCode);
     }
 
 }
