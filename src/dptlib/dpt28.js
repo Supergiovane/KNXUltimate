b209bae572e13bd4ce0ec9e3ede3b32222d5fcb3
Massimo <37666636+Supergiovane@users.noreply.github.com>
2024-01-24 15:49:37 +0100

    1.0.47
    1.0.47

--- src/dptlib/dpt28.js
+++ src/dptlib/dpt28.js
@@ -11,54 +11,53 @@ const knxLog = require('./../KnxLog')
 
 // Write to BUS
 exports.formatAPDU = function (value) {
-    if (typeof value !== 'string') {
-        knxLog.get().error('Must supply a string value. Autoconversion to string')
-        try {
-            value = value.toString()
-        } catch (error) {
-            value = 'DPT Err'
-        }
-    }
-
+  if (typeof value !== 'string') {
+    knxLog.get().error('Must supply a string value. Autoconversion to string')
     try {
-        const buf = Buffer.alloc(14)
-        if (this.subtypeid === '001') buf.write(value, 'utf-8')
-        return buf
+      value = value.toString()
     } catch (error) {
-        return error.message
+      value = 'DPT Err'
     }
+  }
 
+  try {
+    const buf = Buffer.alloc(14)
+    if (this.subtypeid === '001') buf.write(value, 'utf-8')
+    return buf
+  } catch (error) {
+    return error.message
+  }
 }
 
 // Read from BUS
 exports.fromBuffer = function (buf) {
-    //nog length check because this is variable
-    //if (buf.length != 14) {
-    //  knxLog.get().error('DPT28: Buffer should be 14 byte long, got', buf.length)
-    //  return null
-    //}
-    if (this.subtypeid === '001') return buf.toString('utf-8')
+  // nog length check because this is variable
+  // if (buf.length != 14) {
+  //  knxLog.get().error('DPT28: Buffer should be 14 byte long, got', buf.length)
+  //  return null
+  // }
+  if (this.subtypeid === '001') return buf.toString('utf-8')
 }
 
 // DPT28 basetype info
 exports.basetype = {
-    bitlength: 14 * 8,
-    valuetype: 'basic',
-    desc: '14-character string',
-    help:
+  bitlength: 14 * 8,
+  valuetype: 'basic',
+  desc: '14-character string',
+  help:
         `// Send a text to a display
 msg.payload = "Hello UTF-8!"
 return msg;`,
-    helplink: ''
+  helplink: ''
 }
 
 // DPT28 subtypes
 exports.subtypes = {
-    // 28.001 UTF-8 string
-    '001': {
-        use: 'G',
-        desc: 'DPT_String_UTF-8',
-        name: 'UTF-8 string',
-        force_encoding: 'UTF-8'
-    }
+  // 28.001 UTF-8 string
+  '001': {
+    use: 'G',
+    desc: 'DPT_String_UTF-8',
+    name: 'UTF-8 string',
+    force_encoding: 'UTF-8'
+  }
 }
