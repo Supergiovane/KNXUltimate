23a27ab40176af11bdc047cb09c2fe31ae4b22fd
Massimo <37666636+Supergiovane@users.noreply.github.com>
2024-06-14 10:37:52 +0200

    fix: fix DPT10.001 Time error when time passed as string

---
+++ examples/datetime_internaltest.ts
@@ -0,0 +1,22 @@
+import { KNXClientOptions } from "../src/KNXClient";
+import { KNXClientEvents, KNXClient, dptlib } from "../src";
+import { resolve, fromBuffer } from "../src/dptlib";
+
+
+
+async function main() {
+    // Simulazione ricezione da KNX BUS
+    const config = resolve("10.001");
+    let rawArray = new Uint8Array([16, 58, 11]);
+    let _Rawvalue = Buffer.from(rawArray);
+    let jsValue = fromBuffer(_Rawvalue, config);
+    console.log(jsValue);
+
+    // Simulazione invio a KNX
+    const dpt = resolve("10.001");
+    let d = new Date().toString();
+    let toKnxBuff = dpt.formatAPDU(d)
+    console.log(toKnxBuff);
+}
+
+main().catch(console.error);
