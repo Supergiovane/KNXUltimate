6828557567202b81b0f53c767106207e4116b1f8
mandrake-sw <zanetti.sw@gmail.com>
2024-10-22 16:04:03 +0200

    fix(test): return to the previous version of MockKNXServer

--- test/utils/MockKNXServer.ts
+++ test/utils/MockKNXServer.ts
@@ -1,101 +1,68 @@
 import { Socket as UDPSocket } from 'dgram'
 import { Socket as TCPSocket } from 'net'
 import { SnifferPacket } from 'src'
+import { wait } from 'src/utils'
+
+export type ServerOptions = {
+	port?: number
+	host?: string
+	protocol?: 'udp' | 'tcp'
+}
 
 export default class MockKNXServer {
 	private socket: UDPSocket | TCPSocket
 
+	private port: number
+
+	private host: string
+
 	private expectedTelegrams: SnifferPacket[]
 
-	private requestHandlers: Map<string, () => void> = new Map()
+	private lastIndex = 0
 
 	constructor(
 		capturedTelegrams: SnifferPacket[],
 		socket: UDPSocket | TCPSocket,
 	) {
-		console.log('[MOCK] Initializing MockKNXServer')
 		this.expectedTelegrams = capturedTelegrams
 		this.socket = socket
 
-		// For UDP sockets
-		if (socket instanceof UDPSocket) {
-			console.log('[MOCK] Setting up UDP mock server')
-			socket.on('message', (msg: Buffer, rinfo: any) => {
-				console.log('[MOCK] UDP message received:', msg.toString('hex'))
-				this.handleRequest(msg, rinfo)
-			})
+		// intercept write method to capture outgoing data
+		if (socket instanceof TCPSocket) {
+			const originalWrite = socket.write
+			socket.write = (data: Buffer) => {
+				this.onRequest(data)
+				return originalWrite.call(socket, data)
+			}
 		} else {
-			// For TCP sockets
-			console.log('[MOCK] Setting up TCP mock server')
-			socket.on('data', (data: Buffer) => {
-				console.log('[MOCK] TCP data received:', data.toString('hex'))
-				this.handleRequest(data)
-			})
+			const originalSend = socket.send
+			socket.send = (data: Buffer, ...args: any[]) => {
+				this.onRequest(data)
+				return originalSend.call(socket, data, ...args)
+			}
 		}
+	}
 
-		// Set up request handlers
-		this.setupRequestHandlers()
+	// Handles incoming connections and data
+	private async onRequest(data: Buffer) {
+		const requestHex = data.toString('hex') // Convert data to hex string
+		console.log(`Received request: ${requestHex}`)
 
-		console.log(
-			'[MOCK] MockKNXServer initialized with handlers:',
-			Array.from(this.requestHandlers.keys()),
+		// Look up the captured response
+		const resIndex = this.expectedTelegrams.findIndex(
+			(packet, i) => i >= this.lastIndex && packet.request === requestHex,
 		)
-	}
 
-	private setupRequestHandlers() {
-		this.expectedTelegrams.forEach((telegram) => {
-			this.requestHandlers.set(telegram.request, () => {
-				console.log(
-					'[MOCK] Found matching request handler for:',
-					telegram.request,
-				)
-				setTimeout(() => {
-					if (telegram.response) {
-						console.log(
-							'[MOCK] Sending response:',
-							telegram.response,
-						)
-						const responseBuffer = Buffer.from(
-							telegram.response,
-							'hex',
-						)
+		const res = this.expectedTelegrams[resIndex]
 
-						if (this.socket instanceof UDPSocket) {
-							// Emit directly as a message event
-							this.socket.emit('message', responseBuffer, {
-								address: process.env.CI
-									? '127.0.0.1'
-									: '192.168.1.116',
-								port: 3671,
-								family: 'IPv4',
-								size: responseBuffer.length,
-							})
-							console.log('[MOCK] UDP response emitted')
-						} else {
-							// For TCP, emit as data
-							this.socket.emit('data', responseBuffer)
-							console.log('[MOCK] TCP response emitted')
-						}
-					}
-				}, telegram.deltaRes || 0)
-			})
-		})
-	}
-
-	private handleRequest(data: Buffer, rinfo?: any) {
-		const requestHex = data.toString('hex')
-		console.log('[MOCK] Processing request:', requestHex)
-
-		const handler = this.requestHandlers.get(requestHex)
-		if (handler) {
-			console.log('[MOCK] Handler found, executing')
-			handler()
+		if (res && res.response) {
+			await wait(res.deltaRes || 0)
+			this.lastIndex = resIndex
+			console.log(`Sending response: ${res.response}`)
+			const responseBuffer = Buffer.from(res.response, 'hex')
+			this.socket.emit('message', responseBuffer) // Send response as a buffer
 		} else {
-			console.log('[MOCK] No handler found for request:', requestHex)
-			console.log(
-				'[MOCK] Available handlers:',
-				Array.from(this.requestHandlers.keys()),
-			)
+			console.log('No matching response found for this request.')
 		}
 	}
 }
