80834df879c292dd4057d4724a0efe0b369bed30
mandrake-sw <zanetti.sw@gmail.com>
2024-11-25 15:33:41 +0100

    feat: add example for KNXClient usage with logging

---
+++ examples/logging.ts
@@ -0,0 +1,73 @@
+import { KNXClient, KNXClientEvents } from '../src'
+import { module, setLogLevel } from '../src/KnxLog'
+import { wait } from '../src/utils'
+
+const logger = module('KNX-TEST')
+
+async function testKNXClient() {
+    setLogLevel('debug')
+    
+    let heartbeatCount = 0
+    const REQUIRED_HEARTBEATS = 3
+    const HEARTBEAT_INTERVAL = 10
+    const TOTAL_CAPTURE_TIME = (REQUIRED_HEARTBEATS + 2) * HEARTBEAT_INTERVAL * 1000
+
+    logger.info('Starting KNX test...')
+    logger.debug('Test configuration - Required heartbeats: ' + REQUIRED_HEARTBEATS + 
+        ', Heartbeat interval: ' + HEARTBEAT_INTERVAL + ' seconds' +
+        ', Total capture time: ' + TOTAL_CAPTURE_TIME/1000 + ' seconds')
+    
+    logger.info('Discovering KNX interfaces...')
+    const discovered = await KNXClient.discover();
+
+    if (discovered.length === 0) {
+        logger.error('No KNX interfaces found')
+        return
+    }
+
+    logger.info('Found interfaces:', discovered)
+    const [ip, port] = discovered[0].split(':')
+    
+    const client = new KNXClient({
+        ipAddr: ip,
+        ipPort: port,
+        hostProtocol: 'TunnelUDP',
+        connectionKeepAliveTimeout: HEARTBEAT_INTERVAL
+    })
+
+    client.on(KNXClientEvents.response, (host, header, message) => {
+        if (message.constructor.name === 'KNXConnectionStateResponse') {
+            heartbeatCount++
+            logger.info(`Heartbeat ${heartbeatCount}/${REQUIRED_HEARTBEATS} received`)
+        }
+    })
+
+    client.on(KNXClientEvents.connected, async () => {
+        logger.info('Connected to KNX interface')
+        logger.info(`Waiting ${TOTAL_CAPTURE_TIME/1000} seconds to collect ${REQUIRED_HEARTBEATS} heartbeats...`)
+        
+        await wait(TOTAL_CAPTURE_TIME)
+        
+        if (heartbeatCount < REQUIRED_HEARTBEATS) {
+            logger.warn(`Only captured ${heartbeatCount} heartbeats out of ${REQUIRED_HEARTBEATS} required`)
+        }
+        
+        logger.info('Initiating disconnection...')
+        await client.Disconnect()
+    })
+
+    client.on(KNXClientEvents.error, (err) => {
+        logger.error('KNX Error:', err)
+    })
+    
+    client.on(KNXClientEvents.disconnected, (reason) => {
+        logger.info('Disconnected:', reason)
+    })
+
+    logger.info('Connecting to KNX interface...')
+    client.Connect()
+}
+
+testKNXClient().catch(error => {
+    logger.error('Unexpected error occurred:', error)
+})
